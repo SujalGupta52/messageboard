e0e71124275252542325b487afee20f9538570b4
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-07-05 23:36:49 +0530

    Remove example messages

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,6 @@
 import express from "express";
 
 const router = express.Router();
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -74,11 +12,13 @@ router.get("/new", function (req, res, next) {
 });
 
 router.post("/new", function (req, res, next) {
-  messages.push({
-    text: req.body.messageText,
-    user: req.body.author,
-    added: Date.now(),
-  });
+  if (req.body.messageText !== "" && req.body.author !== "")
+    messages.push({
+      text: req.body.messageText,
+      user: req.body.author,
+      added: new Date(),
+    });
+  res.redirect("/");
 });
 
 export default router;
